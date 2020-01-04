8d9cc9e6f98847a7c1a0511eeb3320696d355f79
Kef <a7medosama2222@gmail.com>
2020-01-04 11:20:09 +0200

    Complete basic structure to starboard (no edits/new reactions yet)

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -20,8 +20,35 @@ class Starboard(commands.Cog):
     async def get_entry(self, channel_id, message_id):
         pass
 
-    async def add_entry(self, channel_id, starred_message_id, author_id, count, entry_message_id):
-        pass
+    async def add_entry(self, channel_id, msg, count):
+        print("Creating embed")
+        embed = discord.Embed()
+        embed.set_author(name=str(msg.author), icon_url=msg.author.avatar_url)
+        embed.set_footer(text=f'ID: {msg.id}')
+        embed.timestamp = msg.created_at
+        embed.description = msg.content
+        if msg.attachments:
+            file = msg.attachments[0]
+            if file.url.lower().endswwith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
+                embed.set_image(url=file.url)
+            else:
+                embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
+        
+        embed.add_field(name='Jump to message', value=f'[Jump](https://discordapp.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id})', inline=False)
+        print("Sending message")
+        star_msg = await self.bot.get_channel(config.CHAN_STARBOARD).send(embed=embed)
+        await self.bot.db.starboard.insert_one({
+            'channel_id': channel_id,
+            'message_id': msg.id,
+            'author_id': msg.author.id,
+            'star_message_id': star_msg.id
+        })
+
+    async def remove_entry(self, channel_id, msg, star_message_id):
+        await self.bot.db.starboard.delete_one({'channel_id': channel_id, 'message_id', 'star_message_id': star_message_id})
+        o = discord.Object(id=star_message_id + 1)
+        star_msg = await self.bot.get_channel(config.CHAN_STARBOARD).history(limit=1, before=o).next()
+        await star_msg.delete()
 
     async def reaction_action(self, action, payload):
         if str(payload.emoji) != STAR:
@@ -35,6 +62,10 @@ class Starboard(commands.Cog):
         msg = await channel.history(limit=1, before=o).next()
         
         reaction = discord.utils.find(lambda e: str(e) == STAR, msg.reactions)
+        if not reaction:
+            print('No reactions')
+            return
+        
         count = reaction.count # TODO: when all reactions are removed the reaction object is None
         owner_reacted = discord.utils.get(await reaction.users().flatten(), id=msg.author.id)
         if owner_reacted:
@@ -45,27 +76,17 @@ class Starboard(commands.Cog):
             if count < MIN_REACTIONS:
                 return
             
-            entry = await self.bot.db.starboard.find_one({'channel_id': payload.channel_id, 'starred_message_id': msg.id})
+            entry = await self.bot.db.starboard.find_one({'channel_id': payload.channel_id, 'message_id': msg.id})
             if not entry:
-                print("Creating embed")
-                embed = discord.Embed()
-                embed.set_author(name=str(msg.author), icon_url=msg.author.avatar_url)
-                embed.set_footer(text=f'ID: {msg.id}')
-                embed.timestamp = msg.created_at
-                embed.description = msg.content
-                if msg.attachments:
-                    file = msg.attachments[0]
-                    if file.url.lower().endswwith(('png', 'jpeg', 'jpg', 'gif', 'webp')):
-                        embed.set_image(url=file.url)
-                    else:
-                        embed.add_field(name='Attachment', value=f'[{file.filename}]({file.url})', inline=False)
-                
-                embed.add_field(name='Jump to message', value=f'[Jump](https://discordapp.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id})', inline=False)
-                print("Sending message")
-                await self.bot.get_channel(config.CHAN_STARBOARD).send(embed=embed)
+                self.add_entry(payload.channel_id, msg, count)
                 
         elif action == 'remove':
-            pass
+            print('Entered "remove"')
+            entry = await self.bot.db.starboard.find_one({'channel_id': payload.channel_id, 'message_id': msg.id})
+            if not entry and count >= MIN_REACTIONS:
+                await self.add_entry(payload.channel_id, msg, count)
+            elif entry and count < MIN_REACTIONS:
+                await self.remove_entry(payload.channel_id, msg, entry['star_message_id'])
 
 def setup(bot):
     bot.add_cog(Starboard(bot))
