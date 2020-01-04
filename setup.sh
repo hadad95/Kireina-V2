2c084cf54156a12f7f63ce369b09ef1a3586b2df
Kef <a7medosama2222@gmail.com>
2020-01-04 12:42:36 +0200

    Updated the setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if !command -v python3 &>/dev/null; then
+if ! command -v python3 &>/dev/null; then
     echo 'Installing python3'
     sudo apt-get install python3
 fi
 
-if !command -v pip3 &>/dev/null; then
+if ! command -v pip3 &>/dev/null; then
     echo 'Installing pip'
     sudo apt-get install python3-pip
 fi
 
-if !command -v mongo &>/dev/null; then
+if ! command -v mongo &>/dev/null; then
     echo 'Installing mongodb'
     sudo apt-get install mongodb
 fi
