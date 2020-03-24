0affc4289a3fa3df55a9a92a28ca3c9be1638cd9
Kef <theonlykef@gmail.com>
2020-03-24 18:31:52 +0200

    Added avatar command

--- cogs/main.py
+++ cogs/main.py
@@ -21,5 +21,22 @@ class Main(commands.Cog):
         else:
             await ctx.send(emoji)
 
+    @commands.command(aliases=['av'])
+    async def avatar(self, ctx, member=None):
+        url = None
+        name = None
+        if member:
+            url = str(member.avatar_url)
+            name = str(member)
+        else:
+            url = str(ctx.author.avatar_url)
+            name = str(ctx.author)
+
+        embed = discord.Embed()
+        embed.title = f"{name}'s avatar"
+        embed.colour = discord.Colour.blue()
+        embed.set_image(url=url)
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Main(bot))
