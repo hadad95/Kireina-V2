4b5d93a8cb5f71be65f9e96aefd9815acb4ed9d1
Kef <theonlykef@gmail.com>
2020-12-18 16:20:53 +0200

    Stopped giving xp if user is alone in a vc channel

--- cogs/levels.py
+++ cogs/levels.py
@@ -28,7 +28,7 @@ class Levels(commands.Cog):
         guild = self.bot.get_guild(config.GUILD)
         for vc in guild.voice_channels:
             for member in vc.members:
-                if member.voice and not member.voice.afk:
+                if member.voice and not member.voice.afk and len(member.voice.channel.members) > 1:
                     await self.add_xp(member, None)
     
     async def level_up(self, member, level, channel):
